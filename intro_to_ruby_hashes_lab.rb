716f5c5e6523fd361f79fbc48126e13967239d0d
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-20 19:32:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,20 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  new_hash = {railroads: Array.new, pieces: "4", rent_in_dollars: Hash.new, names: {}}
+  new_hash = {
+    railroads: Array.new,
+    pieces: "4",
+    rent_in_dollars: Hash.new,
+    names: {}}
 end
 
 
 def monopoly_with_third_tier
+  new_hash = {
+    railroads: Array.new,
+    pieces: "4",
+    rent_in_dollars: Hash.new,
+    names: {}}
 
 end
 
